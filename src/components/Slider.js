f66cb0b80f0d4232d89741a2426372ba2eff03b7
SayHelloToKam <sayhello@kamilwatson.com>
2022-10-10 13:43:04 -0400

    add slider functionality, react-icons

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -1,8 +1,47 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
+import '../styles/slider.css'
+import { MdKeyboardArrowLeft, MdKeyboardArrowRight } from "react-icons/md";
+
 
 function Slider({apiKey, baseUrl}) {
+
+const [upcomingMovies, setUpcomingMovies]=useState([]);
+const [index, setIndex]=useState(0)
+
+    useEffect(() => {
+     axios.get(`${baseUrl}/movie/upcoming?api_key=${apiKey}`)
+     .then(res=>{
+        setUpcomingMovies(res.data.results)
+     })
+     .catch(err=>console.log(err))
+    }, [])
+
+const sliderStyle = {
+    height:"60vh",
+    width:"100%",
+    backgroundImage:`url("https://image.tmdb.org/t/p/original/${upcomingMovies[index]?.backdrop_path}")`,
+    backgroundRepeat: "no-repeat",
+    backgroundSize: "cover",
+    backgroundPosition: "center",
+    position: "relative",
+}
+
+const handleRight=()=>{
+    setIndex(index+1)
+}
+
+const handleLeft=()=>{
+    setIndex(index-1)
+}
+
+
   return (
-    <div>Slider</div>
+    <div className='slider-container' style={sliderStyle}>
+        <div className='slider-overlay'></div>
+        <MdKeyboardArrowLeft className='left-arrow' onClick={handleLeft}/>
+        <MdKeyboardArrowRight className='right-arrow'onClick={handleRight}/>
+    </div>
   )
 }
 
